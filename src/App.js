346abd4957e186c7a7cfd48b9258ed37597bc928
Elijah Littell <elittell214@gmail.com>
2022-10-04 03:47:36 -0400

    Added stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+import React, { useEffect } from "react";
+import "./css/app.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Link } from "react-router-dom";
+
+import animate from "./modules/animate";
+
 function App() {
+  useEffect(() => {
+    animate();
+  });
+
   return (
-    <div className="App">
-      <header>
-        <h1>Carters Website</h1>
-      </header>
+    <div className="app">
+      <Header />
+
       <main>
-        <h2>Body</h2>
+        <section>
+          <Link to="/vote" id="vote">
+            Vote
+          </Link>
+          <Link to="/philosophy" id="philosophy">
+            Philosophy
+          </Link>
+          <Link to="/merch" id="merch">
+            Merch
+          </Link>
+          <Link to="/content" id="contact">
+            Content
+          </Link>
+        </section>
       </main>
-      <footer>
-        <p>&copy; 2022;</p>
-      </footer>
+
+      <Footer />
     </div>
   );
 }
