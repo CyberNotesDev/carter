45c3e13e33c486f6f9afe13156b0fc3a7467e83d
Elijah Littell <elittell214@gmail.com>
2022-10-04 03:46:36 -0400

    Added react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/carter",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "vote",
+    element: <div>Vote</div>,
+  },
+  {
+    path: "philosophy",
+    element: <div>Philosophy</div>,
+  },
+  {
+    path: "merch",
+    element: <div>Merch</div>,
+  },
+  {
+    path: "Content",
+    element: <div>content</div>,
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
