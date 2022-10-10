790c52ffecc7072ca1f864440e92b7f5253ec85f
Elijah Littell-Sivley <elittell214@gmail.com>
2022-10-10 09:43:40 -0400

    Create Vote.js

---
+++ src/components/Vote.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Article } from "./article";
+import styles from "./vote.modules.css";
+
+function Vote() {
+
+  return (
+    <div>
+      <Article image={} title={} description={} />
+    </div>
+  )
+
+}
