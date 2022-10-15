b02b4180c2957b4eb7fdd2de5c4e383e33159c9e
Elijah Littell <elittell214@gmail.com>
2022-10-15 01:11:23 -0400

    Deleted the original names

--- src/index.js
+++ src/index.js
@@ -2,46 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import { ErrorPage } from "./ErrorPage";
-import { Vote } from "./components/Vote";
+import Vote from "./components/Vote";
+import Links from "./components/Links"
 
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/carter",
-    element: <App />,
-    errorElement: <p>404</p>,
-    children: [
-      {
-        path: "/carter",
-        element: <Home />
-      },
-      {
-        path: "carter/vote",
-        element: <Vote />,
-      },
-      {
-        path: "carter/philosophy",
-        element: <div>Philosophy</div>,
-      },
-      {
-        path: "carter/merch",
-        element: <div>Merch</div>,
-      },
-      {
-        path: "carter/content",
-        element: <div>content</div>,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<App />}>
+      <Route
+        path="carter"
+        element={<Links />}
+      />
+      <Route path="carter/vote" element={<Vote />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(-
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
